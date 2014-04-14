c1495da2984d3651f1955c7138a972a7e9644e9a
Jonas Sticha <jonas.sticha@bmw-carit.de>
2014-04-14 14:11:40 +0200

    Add license header to source files

--- timer_tests/src/RosOneShotTimerTestsFixture.cpp
+++ timer_tests/src/RosOneShotTimerTestsFixture.cpp
@@ -1,3 +1,11 @@
+/**
+* Copyright (C) 2014, BMW Car IT GmbH
+* Author: Jonas Sticha (Jonas.Sticha@bmw-carit.de)
+*
+* This software is licensed under BSD 3-clause License
+* (see http://spdx.org/licenses/BSD-3-Clause).
+**/
+
 #include "RosOneShotTimerTestsFixture.h"
 #include "Logger.h"
 #include "OneShotLatencyMeasurer.h"
