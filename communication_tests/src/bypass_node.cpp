e948871ab851910dfc48f72155ed74feeaf9a681
2quarius <863115889@qq.com>
2021-03-16 19:28:34 +0800

    add bypass node

--- communication_tests/src/bypass_node.cpp
+++ communication_tests/src/bypass_node.cpp
@@ -8,10 +8,13 @@ int main(int argc, char* argv[])
     ros::NodeHandle n;
 
     ros::Publisher pub = n.advertise<communication_tests::timestamp_msg>("communication_test", 1000);
+    int cnt = 0;
     ros::Subscriber sub = n.subscribe<communication_tests::timestamp_msg>("bypass", 1000,
         [&](const communication_tests::timestamp_msg::ConstPtr& msg)
         {
+            ROS_INFO("recieve %d th msg", cnt);
             pub.publish(*msg);
+            cnt++;
         });
     ros::spin();
 }
